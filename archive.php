9a4fe215280b144b8617302b01bafad4d5cf8783
daltonnyx <quytruong851@gmail.com>
2017-05-28 22:36:49 +0700

    Update trang con

--- archive.php
+++ archive.php
@@ -13,50 +13,67 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<?php
 			if ( have_posts() ) : ?>
-			
+			<div class="title-bar">
+				<div class="mui-container">
+					<div class="mui-row">
+						<div class="mui-col-md-12">
+							<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+						</div>
+						<div class="mui-col-md-12">
+							<?php the_breadcrumb(); ?>
+						</div>
+					</div>
+				</div>
+			</div>
 			<div class="mui-container">
 			<div class="mui-row">
 
         <div class="mui-col-md-12 mui-xs-12">
-            <div class="mui-row">
-            <div class="title-bar">
-			<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
-			</div>
-					<div class="cards-list">
+
+					<div class="news-list">
 
 						<?php
-						/* Start the Loop */
+                        /* Start the Loop */
+                        $delay_count = 0;
 						while ( have_posts() ) : the_post();
 
 							/*
 							 * Include the Post-Format-specific template for the content.
 							 * If you want to override this in a child theme, then include a file
 							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-							 */
+                             */
 							get_template_part( 'components/post/content', 'card-list' );
-
+                            $delay_count = $delay_count == 2 ? 0 : ++$delay_count;
 						endwhile;
 						?>
 					</div>
 					<div class="navigation">
 						<?php wp_pagenavi(); ?>
-                    </div>
-</div>
+					</div>
 				</div>
+				<?php//	get_sidebar(); ?>
 			</div>
 		</div>
 		</div>
 			<?php
 
 		else : ?>
-								
+			<div class="title-bar">
+				<div class="mui-container">
+					<div class="mui-row">
+						<div class="mui-col-md-12">
+							<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+						</div>
+						<div class="mui-col-md-12 mui--text-right">
+							<?php the_breadcrumb(); ?>
+						</div>
+					</div>
+				</div>
+			</div>
 			<div class="mui-container">
 				<div class="mui-row">
+					<?php//	get_sidebar(); ?>
 					<div class="mui-col-md-12 mui-xs-12 main-content">
-                    <div class="title-bar">
-                        <?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
-                    </div>
-
 						<?php	get_template_part( 'components/post/content', 'none' ); ?>
 
 		<?php endif; ?>
--- components/post/content-card-list.php
+++ components/post/content-card-list.php
@@ -6,17 +6,20 @@
  *
  * @package vinabits
  */
-
+global $delay_count;
+$delay = 'delay-'. $delay_count * 100;
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('main-content card-view'); ?>><div class="mui-row">
-    <div class="mui-col-xs-12 mui-col-md-12">
-       <a href="<?php echo get_permalink(); ?>"><?php the_post_thumbnail('card-thumb',array('class' => 'img-responsive', 'alt' => get_the_title())); ?></a>
+<article id="post-<?php the_ID(); ?>" <?php post_class('main-content card-view has-animation fadeInUp '. $delay); ?>>
+    <div class="mui-col-xs-12 mui-col-md-12 thumb-container">
+       <a href="<?php echo get_permalink(); ?>"><?php the_post_thumbnail('full',array('class' => 'img-responsive', 'alt' => get_the_title())); ?></a>
     </div>
-    <div class="mui-col-xs-12 mui-col-md-12">
+    <div class="mui-col-xs-12 mui-col-md-12 post-container">
         <header class="entry-header">
         <a href="<?php echo get_permalink(); ?>"><h3 class="entry-title"><?php echo get_the_title(); ?></h3></a>
         </header>
-        
+        <div class="entry-desc">
+            <?php echo get_my_excerpt(40); ?>
+        </div>
     </div>
-</div></article><!-- #post-## -->
+</article><!-- #post-## -->
