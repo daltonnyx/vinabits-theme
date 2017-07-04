513b869d3513b19983f077214bc93af768afea88
quytruong851@gmail.com <quytruong851@gmail.com>
2017-07-05 00:06:25 +0700

    new news template for promo

--- components/post-widget/news-promo.php
+++ components/post-widget/news-promo.php
@@ -1,8 +1,8 @@
 <div class="news-carousel owl-carousel">
 <?php $i = 0; ?>
 <?php if($news->have_posts()) : while($news->have_posts()) : $news->the_post(); ?>
-    <div class="news-default-item has-animation fadeInUp smooth delay-<?php echo $i; ?>">
-
+    <div class="news-default-item ">
+    <h3 class="news-title"><?php echo get_the_title(); ?></h3>
     <div class="thumb-container">
       <?php if($has_thumbnail) { ?>
         <?php the_post_thumbnail('post-carousel', array(
@@ -10,12 +10,14 @@
           'class' => 'img-responsive horizonal-thumb'
         )); ?>
       <?php } ?>
+      <div class="promo">
+        <span>Quà tặng</span>
+        <span class="value"><?php echo get_post_meta(get_the_ID(), '_vnb_qua_tang',true); ?></span>
+      </div>
     </div>
-    <div class="news-container">
-      <h3 class="news-title"><?php echo get_the_title(); ?></h3>
-      <a class="vnb-btn" href="<?php echo get_permalink(); ?>">
-        Xem thêm <i class="fa fa-caret-right" aria-hidden="true"></i>
-      </a>
+    <div class="promo-category">
+        <?php $terms = wp_get_post_terms(get_the_ID(), 'promo_cat'); ?>
+        <a href="<?php echo get_term_link($terms[0], 'promo_cat'); ?>" class="term-link"><?php echo $terms[0]->name; ?></a>
     </div>
     
   </div>
